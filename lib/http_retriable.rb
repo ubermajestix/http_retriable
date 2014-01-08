627faec5c73821a2718fc3ce231a3e4ca448612f
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2014-01-08 10:43:57 -0800

    Quick retries don't count towards overall retries, don't grow sleep quick as fast

--- lib/http_retriable.rb
+++ lib/http_retriable.rb
@@ -22,18 +22,23 @@ module HttpRetriable
     backoff = !should_sleep # if sleep is provided by the user, don't backoff
     exceptions = options.fetch(:exceptions, default_exceptions)
     quick_retries = options.fetch(:quick_retries, 2)
+    logger = options.fetch(:logger, Logger.new($stderr))
 
     retried = 0
+    quick_retried = 0
     seconds_to_sleep = should_sleep ? should_sleep : 2
     begin
       yield
     rescue *exceptions => e
-      retried += 1
       if backoff
-        if retried < quick_retries
+        if quick_retried < quick_retries
+          quick_retried += 1
+          logger.debug("[HTTP_RETRIABLE] quick retry: #{quick_retried}/#{quick_retries}") 
           retry
         elsif retried < retries
-          seconds_to_sleep = seconds_to_sleep ** 2
+          retried += 1
+          seconds_to_sleep = 2 ** retried
+          logger.debug("[HTTP_RETRIABLE] backoff retry: #{retried}/#{retries} sleeping for: #{seconds_to_sleep}s") 
           sleep seconds_to_sleep
           retry
         else
@@ -41,6 +46,8 @@ module HttpRetriable
         end
       else
         if retried < retries
+          logger.debug("[HTTP_RETRIABLE] retry: #{retried}/#{retries} sleeping for: #{seconds_to_sleep}s") 
+          retried += 1
           sleep seconds_to_sleep
           retry
         else
--- lib/http_retriable/version.rb
+++ lib/http_retriable/version.rb
@@ -1,3 +1,3 @@
 module HttpRetriable
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
